c4ba51baba910aec38548ae023026c567a6e6510
fatayri <fatayri@gmail.com>
2023-03-15 14:32:25 +0100

    add confederations

--- tfmkt/spiders/confederations.py
+++ tfmkt/spiders/confederations.py
@@ -3,8 +3,8 @@ from scrapy.shell import inspect_response # required for debugging
 
 default_confederation_hrefs = [
   '/wettbewerbe/europa']
-#  '/wettbewerbe/europa?page=2',
-#  'wettbewerbe/amerika'
+  '/wettbewerbe/europa?page=2',
+  'wettbewerbe/amerika'
 #]
 
 class ConfederationsSpider(BaseSpider):
