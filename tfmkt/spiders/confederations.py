c24fe152ff129b5897975062e56b30a8ae919f67
fatayri <fatayri@gmail.com>
2023-03-15 14:37:20 +0100

    fix typo

--- tfmkt/spiders/confederations.py
+++ tfmkt/spiders/confederations.py
@@ -4,7 +4,7 @@ from scrapy.shell import inspect_response # required for debugging
 default_confederation_hrefs = [
   '/wettbewerbe/europa',
   '/wettbewerbe/europa?page=2',
-  'wettbewerbe/amerika'
+  '/wettbewerbe/amerika'
 ]
 
 class ConfederationsSpider(BaseSpider):
