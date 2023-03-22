a02b47579029b625f5261d4575221102f710fa86
fatayri <fatayri@gmail.com>
2023-03-22 16:00:25 +0100

    collect_players

---
+++ results/get_all_players.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+if os.path.exists("parsed_hrefs.txt"):
+    os.remove("parsed_hrefs.txt")
+
+directories = [x.path for x in os.scandir('../results') if x.is_dir()]
+players = [x + '/players.json' for x in directories]
+
+all_hrefs = []
+for file in players:
+	with open(file) as f:
+		print(file)
+		for line in f:
+			x = json.loads(line)
+			all_hrefs.append(x)
+
+with open('all_players.json', 'w') as f:
+	for href in all_hrefs:
+		f.write(f"{href}\n")
+
+print(len(all_hrefs))
+
